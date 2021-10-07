e2e86d387da2deaa537127552eeb1c4449692bf6
Michel Metran <michelmetran@gmail.com>
2021-10-07 02:47:56 -0300

    Aeee

--- setup.py
+++ setup.py
@@ -10,16 +10,17 @@ requirements = [
 
 setup(
     name='traquitanas',  # Nome (não precisa ser o nome do repositório, nem de qualquer pasta...)
-    version='1.0.6',    
+    version='1.0.6',
     author='Michel Metran',
     author_email='michelmetran@gmail.com',
     description='Small Defs...',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/open-dsa/gerador_endereco',
-    #package_dir = {'': 'src'}, # Our packages live under src but src is not a package itself    
+    package_dir = {'': 'src'}, # Our packages live under src but src is not a package itself    
     #packages=find_packages(),
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages('src.*', exclude=['*tests*']),
+    #include_package_data=False, # https://stackoverflow.com/questions/61274967/why-cant-i-exclude-tests-directory-from-my-python-wheel-using-exclude
     py_modules = ['traquitanas'],
     install_requires=requirements,
     keywords='python, endereço aleatório, address',
