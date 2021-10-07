b84284d90d9db50d3be701cf7f574a883bc7151e
Michel Metran <michelmetran@gmail.com>
2021-10-07 02:48:22 -0300

    a

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name='traquitanas',  # Nome (não precisa ser o nome do repositório, nem de qualquer pasta...)
-    version='1.0.6',
+    version='1.0.7',
     author='Michel Metran',
     author_email='michelmetran@gmail.com',
     description='Small Defs...',
