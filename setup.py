f2d43ddb0aa7fa122c804fb0899315c88ad5c56a
Michel Metran <michelmetran@gmail.com>
2021-10-07 02:06:17 -0300

    aaa

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ requirements = [
 
 setup(
     name='traquitanas',  # Nome (não precisa ser o nome do repositório, nem de qualquer pasta...)
-    version='1.0.4',
+    version='1.0.5',
     author='Michel Metran',
     author_email='michelmetran@gmail.com',
     description='Small Defs...',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/open-dsa/gerador_endereco',
-    package_dir = {'': 'src'}, # Our packages live under src but src is not a package itself
-    #packages=find_packages('src', exclude=['tests']),
+    #package_dir = {'': 'src'}, # Our packages live under src but src is not a package itself    
     #packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     py_modules = ['traquitanas'],
     install_requires=requirements,
     keywords='python, endereço aleatório, address',
