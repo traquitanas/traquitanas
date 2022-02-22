e84ddeffb684b9dfb02e010b5bf513aa359df981
Michel Metran <michelmetran@gmail.com>
2022-02-22 12:06:38 -0300

    Ajusts

--- src/traquitanas/__init__.py
+++ src/traquitanas/__init__.py
@@ -1,4 +1,4 @@
-from traquitanas.geo import converts
+# https://stackoverflow.com/questions/4902974/python-import-subpackage-from-a-package-not-working
+from traquitanas.geo import converts, layers
 
-__all__ = ['geo', 'converts']
-
+__all__ = ['geo', 'converts', 'layers']
